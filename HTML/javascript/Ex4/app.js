e294359f7f5933c4dc1ed78aacadb577094eada0
mvn_unowhy_mint <maxouja0603@gmail.com>
2022-03-30 19:36:03 +0200

    exos html + snake sur grille

--- HTML/javascript/Ex4/app.js
+++ HTML/javascript/Ex4/app.js
@@ -1,8 +1,25 @@
+var val = 5
+
 window.onload=function() {
 const jauge = new RGraph.Gauge({
     id: 'jauge',
     min:0,
     max:10,
-    value:5
+    value: val,
+    options: {
+        adjustable: true
+    }
 }).draw();
+}
+
+function up() {
+    val += 1
+    
+}
+    
+
+
+
+function down() {
+    jauge.value -= 1
 }
